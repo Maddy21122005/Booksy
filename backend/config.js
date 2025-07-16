baa70e2f308afd8313e8feeffd96370ea31c588f
Madhav Poddar <madhav.poddar.met23@itbhu.ac.in>
2025-07-16 11:01:48 +0530

    Initial commit – personalized

---
+++ backend/config.js
@@ -0,0 +1,5 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://madhavpoddar2005:madhav1452@cluster0.bexqlxd.mongodb.net/bookstore?retryWrites=true&w=majority&appName=Cluster0';
+
